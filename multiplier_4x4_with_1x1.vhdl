236190b703a32453884c5aaaaaa8726f26253689
nimamakhmali <nmkhmly790@gmail.com>
2024-12-19 14:04:12 +0100

    update without debugging

--- multiplier_4x4_with_1x1.vhdl
+++ multiplier_4x4_with_1x1.vhdl
@@ -5,3 +5,23 @@ entity multiplier_4x4 is
     port (
         
     );
+end entity;
+
+architecture structural of multiplier_4x4 is
+    
+    component multiplier_1x1 is
+        port (
+
+        );
+    end component multiplier_1x1;
+    
+    component full_adder is
+        port (
+
+        );
+    end component full_adder;
+    
+    signal ;
+    begin
+
+end architecture structural;       
--- tb_multiplier_4x4_with_1x1.vhdl
+++ tb_multiplier_4x4_with_1x1.vhdl
@@ -4,3 +4,4 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity tb_multiplier_4x4_with_1x1 is
     end tb_multiplier_4x4_with_1x1;
     
+    
